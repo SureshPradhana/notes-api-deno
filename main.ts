b3e925facf77f31661cded6e09342f058b30994a
SureshPradhana <sureshpradhana19@gmail.com>
2024-06-07 19:18:15 +0530

    chore: rate limiter

--- main.ts
+++ main.ts
@@ -27,10 +27,21 @@ import bycardId from "./routes/bycardId.ts";
 import forgotPassword from "./routes/forgotPassword.ts";
 import resetPassword from "./routes/resetPassword.ts";
 const kv = await Deno.openKv();
+import { rateLimiter } from "npm:hono-rate-limiter";
+
+const limiter = rateLimiter({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c) => "<unique_key>", // Method to generate custom identifiers for clients.
+  // store: ... , // Redis, MemoryStore, etc. See below.
+});
 
 const app = new Hono();
 
 app.use(cors());
+
+app.use(limiter);
 async function checkExistence() {
   const exists = await kv.get(["my-key"]);
   if (
